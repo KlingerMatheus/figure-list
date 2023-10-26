3e3cd406de34142cd5e0f1fffc9039a8307d4034
KlingerMatheus <klinger.matheus@gitstart.dev>
2023-10-26 14:19:20 -0300

    update workflow

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           type="textarea"
           onChange={(e) => setStickers(e.target.value)}
           id="textInput"
-          placeholder="type"
+          placeholder="type here again"
         />
       </div>
       <Result value={stickers} />
